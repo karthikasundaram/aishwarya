641700ceb5b2be22fc9e88799173ed67e47a3a45
karthikasundaram <karthisri01@gmail.com>
2021-07-27 00:59:05 +0530

    selenium task done

--- src/org/com/GitSession.java
+++ src/org/com/GitSession.java
@@ -3,7 +3,12 @@ package org.com;
 public class GitSession {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("git session day first class");
+
+		String s = "selenium";
+		System.out.println(s.length());
+
 	}
+
 }
