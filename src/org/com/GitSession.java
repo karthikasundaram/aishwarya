343fddcbf8939f00e419eb9da31e2e768a746193
karthikasundaram <karthisri01@gmail.com>
2021-07-27 01:28:31 +0530

    java task is done

--- src/org/com/GitSession.java
+++ src/org/com/GitSession.java
@@ -1,9 +1,16 @@
 package org.com;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class GitSession {
 
 	public static void main(String[] args) {
 		
 		System.out.println("git session day first class");
+     List<String> s= new LinkedList<>();
+     s.add("selenium");
+	
+	
 	}
 }
